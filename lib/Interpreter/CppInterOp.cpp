2690797f28d54a199e56f84e8456816c7644a754
Anutosh Bhat <87052487+anutosh491@users.noreply.github.com>
2024-10-29 18:23:42 +0530

    Update CppInterOp.cpp

--- lib/Interpreter/CppInterOp.cpp
+++ lib/Interpreter/CppInterOp.cpp
@@ -8,9 +8,7 @@
 //------------------------------------------------------------------------------
 
 #include "clang/Interpreter/CppInterOp.h"
-
 #include "Compatibility.h"
-
 #include "clang/AST/CXXInheritance.h"
 #include "clang/AST/Decl.h"
 #include "clang/AST/DeclCXX.h"
@@ -28,12 +26,10 @@
 #include "clang/Sema/Redeclaration.h"
 #endif
 #include "clang/Sema/TemplateDeduction.h"
-
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_os_ostream.h"
-
 #include <sstream>
 #include <string>
 
@@ -159,7 +155,6 @@ namespace Cpp {
 
   bool IsAggregate(TCppScope_t scope) {
     Decl *D = static_cast<Decl*>(scope);
-
     // Aggregates are only arrays or tag decls.
     if (ValueDecl *ValD = dyn_cast<ValueDecl>(D))
       if (ValD->getType()->isArrayType())
