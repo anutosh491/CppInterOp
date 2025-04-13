135451034de3cd0e27d787c7a42f0f5c1ca54c88
Anutosh Bhat <andersonbhat491@gmail.com>
2025-04-13 17:24:39 +0530

    Update CppInterOpInterpreter.h

--- lib/Interpreter/CppInterOpInterpreter.h
+++ lib/Interpreter/CppInterOpInterpreter.h
@@ -152,8 +152,8 @@ public:
     llvm::InitializeAllAsmPrinters();
 
     std::vector<const char*> vargs(argv + 1, argv + argc);
-    vargs.push_back("-include");
-    vargs.push_back("new");
+    // vargs.push_back("-include");
+    // vargs.push_back("new");
     auto CI = compat::createClangInterpreter(vargs);
     if (!CI) {
       llvm::errs() << "Interpreter creation failed\n";
